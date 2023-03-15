b7e1f146097452bfaaf0025095b5699b25d52c41
Shubham Patidar <skpatidar222@gmail.com>
2023-03-15 11:52:18 +0530

    Added about us nad dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -8,7 +9,8 @@ export function App() {
       <Navbar title="TextUlits" aboutText="About" />
       {/* <Navbar /> */}
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" />
+        {/* <TextForm heading="Enter the text to analyze" /> */}
+        <About />
       </div>
     </>
   );
